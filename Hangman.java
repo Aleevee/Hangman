e3c274dcfd5f17ccd9ed75982ff22d3260984660
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-09 16:24:47 -0500

    Create Hangman.java

---
+++ Hangman.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Hangman{
+    public static void main(String[] args){
+
+        System.out.println("_____");
+        System.out.println("|/   |");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|_______");
+
+        System.out.println("Ready to play Hangman?");
+    }
+}
