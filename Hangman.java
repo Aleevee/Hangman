0849e1119d70647b3b2c6fd7e66fa8937eeee4c2
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-09 16:37:12 -0500

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -12,5 +12,16 @@ public class Hangman{
         System.out.println("|_______");
 
         System.out.println("Ready to play Hangman?");
+
+         Scanner s = new Scanner(System.in);
+        System.out.println("Y/N?");
+
+        String answer = s.nextLine();
+
+        switch (answer) {
+            case "N", "n" -> System.out.println("See you next time!");
+            case "Y", "y" -> System.out.println("Let's begin!");
+        }
     }
 }
+
